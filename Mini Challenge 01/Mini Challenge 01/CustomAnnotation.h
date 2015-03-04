4e6396b3d6881232df2bcb38e80ca0a251128a41
William Cho <williamhjcho@gmail.com>
2015-03-04 12:44:20 -0300

    added first functionalities, no interactions between views

---
+++ Mini Challenge 01/Mini Challenge 01/CustomAnnotation.h
@@ -0,0 +1,13 @@
+//
+//  CustomAnnotation.h
+//  Mini Challenge 01
+//
+//  Created by William Hong Jun Cho on 3/3/15.
+//  Copyright (c) 2015 Nerf. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CustomAnnotation : NSObject
+
+@end
---
+++ Mini Challenge 01/Mini Challenge 01/CustomAnnotation.m
@@ -0,0 +1,13 @@
+//
+//  CustomAnnotation.m
+//  Mini Challenge 01
+//
+//  Created by William Hong Jun Cho on 3/3/15.
+//  Copyright (c) 2015 Nerf. All rights reserved.
+//
+
+#import "CustomAnnotation.h"
+
+@implementation CustomAnnotation
+
+@end
